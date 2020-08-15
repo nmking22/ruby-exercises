6f9346c29edecb423679c45a748d6dde8376aa68
Nick King <nickmaxking@gmail.com>
2020-08-15 13:10:39 -0600

    Add find, map, select exercises

--- enumerables/exercises_1/map_pattern_test.rb
+++ enumerables/exercises_1/map_pattern_test.rb
@@ -14,55 +14,87 @@ class MapPatternTest < Minitest::Test
   end
 
   def test_doubles
-    skip
+    # skip
     numbers = [1, 2, 3, 4, 5]
     doubles = []
     numbers.each do |number|
-      # Your code goes here
+      doubles << number * 2
     end
     assert_equal [2, 4, 6, 8, 10], doubles
   end
 
   def test_squares
-    skip
+    # skip
     numbers = [1, 2, 3, 4, 5]
     squares = []
-    # Your code goes here
+    numbers.each do |number|
+      squares << number * number
+    end
     assert_equal [1, 4, 9, 16, 25], squares
   end
 
   def test_lengths
-    skip
+    # skip
     names = ["alice", "bob", "charlie", "david", "eve"]
-    # Your code goes here
+    lengths = []
+    names.each do |name|
+      lengths << name.length
+    end
     assert_equal [5, 3, 7, 5, 3], lengths
   end
 
   def test_normalize_zip_codes
-    skip
+    # skip
     numbers = [234, 10, 9119, 38881]
-    # Your code goes here
+    zip_codes = []
+    numbers.each do |number|
+      numbers_array = number.to_s.split("")
+      until numbers_array.length == 5
+        numbers_array.unshift("0")
+      end
+      numbers_array = numbers_array.join
+      zip_codes << numbers_array
+    end
     assert_equal ["00234", "00010", "09119", "38881"], zip_codes
   end
 
   def test_backwards
-    skip
+    # skip
     names = ["alice", "bob", "charlie", "david", "eve"]
-    # Your code goes here
+    backwards = []
+    names.each do |name|
+      backwards << name.reverse
+    end
     assert_equal ["ecila", "bob", "eilrahc", "divad", "eve"], backwards
   end
 
   def test_words_with_no_vowels
-    skip
+    # skip
     words = ["green", "sheep", "travel", "least", "boat"]
-    # Your code goes here
+    vowels = ["a", "e", "i", "o", "u", "y"]
+    without_vowels = []
+    words.each do |word|
+      word_array = word.split("")
+      word_without_vowels = []
+      word_array.each do |letter|
+        unless vowels.include? letter
+          word_without_vowels << letter
+        end
+      end
+      without_vowels << word_without_vowels.join
+    end
     assert_equal ["grn", "shp", "trvl", "lst", "bt"], without_vowels
   end
 
   def test_trim_last_letter
-    skip
+    # skip
     animals = ["dog", "cat", "mouse", "frog", "platypus"]
-    # Your code goes here
+    trimmed = []
+    animals.each do |animal|
+      animal_array = animal.split("")
+      animal_array.pop
+      trimmed << animal_array.join
+    end
     assert_equal ["do", "ca", "mous", "fro", "platypu"], trimmed
   end
 
--- enumerables/exercises_1/select_test.rb
+++ enumerables/exercises_1/select_test.rb
@@ -15,71 +15,108 @@ class SelectTest < Minitest::Test
   def test_pick_odd_numbers
     numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     odds = numbers.select do |number|
-      # Your code goes here
+      number.odd?
     end
     assert_equal [1, 3, 5, 7, 9], odds
   end
 
   def test_pick_words_with_three_letters
-    skip
+    # skip
     words = ["pill", "bad", "finger", "cat", "blue", "dog", "table", "red"]
-    # Your code goes here
+    selected = words.select do |word|
+      word.length == 3
+    end
     assert_equal ["bad", "cat", "dog", "red"], selected
   end
 
   def test_pick_words_with_more_than_three_letters
-    skip
+    # skip
     words = ["pill", "bad", "finger", "cat", "blue", "dog", "table", "red"]
-    # Your code goes here
+    selected = words.select do |word|
+      word.length > 3
+    end
     assert_equal ["pill", "finger", "blue", "table"], selected
   end
 
   def test_pick_words_ending_in_e
-    skip
+    # skip
     words = ["are", "you", "strike", "thinking", "belt", "piece", "warble", "sing", "pipe"]
-    # Your code goes here
+    selected = words.select do |word|
+      word_array = word.split("")
+      if word_array[-1] == "e"
+        word
+      end
+    end
     assert_equal ["are", "strike", "piece", "warble", "pipe"], selected
   end
 
   def test_pick_words_ending_in_ing
-    skip
+    # skip
     words = ["bring", "finger", "drought", "singing", "bingo", "purposeful"]
-    # Your code goes here
+    selected = words.select do |word|
+      word_array = word.split("")
+      if word_array[-1] == "g" && word_array[-2] == "n" && word_array[-3] == "i"
+        word
+      end
+    end
     assert_equal ["bring", "singing"], selected
   end
 
   def test_pick_words_containing_e
-    skip
+    # skip
     words = ["four", "red", "five", "blue", "pizza", "purple"]
-    # Your code goes here
+    selected = words.select do |word|
+      word_array = word.split("")
+      if word_array.include? "e"
+        word
+      end
+    end
     assert_equal ["red", "five", "blue", "purple"], selected
   end
 
   def test_pick_dinosaurs
-    skip
+    # skip
     animals = ["tyrannosaurus", "narwhal", "eel", "achillesaurus", "qingxiusaurus"]
-    # Your code goes here
+    dinosaurs = animals.select do |animal|
+      animal_array = animal.split("")
+      if animal_array[-1] == "s" && animal_array[-2] == "u"
+        animal
+      end
+    end
     assert_equal ["tyrannosaurus", "achillesaurus", "qingxiusaurus"], dinosaurs
   end
 
   def test_pick_floats
-    skip
+    # skip
     numbers = [3, 1.4, 3.5, 2, 4.9, 9.1, 8.0]
-    # Your code goes here
+    floats = numbers.select do |number|
+      number = number.to_s
+      if number.include? "."
+        number.to_f
+      end
+    end
     assert_equal [1.4, 3.5, 4.9, 9.1, 8.0], floats
   end
 
   def test_pick_arrays
-    skip
+    # skip
     elements = ["CAT", ["dog"], 23, [56, 3, 8], "AIMLESS", 43, "butter"]
-    # Your code goes here
+    arrays = elements.select do |element|
+      if element.kind_of?(Array)
+        element
+      end
+    end
     assert_equal [["dog"], [56, 3, 8]], arrays
   end
 
   def test_pick_hashes
-    skip
+    # skip
     elements = ["cat", {:dog=>"fido"}, 23, {:stuff=>"things"}, "aimless", 43]
-    # Your code goes here
+    hashes = elements.select do |element|
+      if element.kind_of?(Hash)
+        element
+      end
+    end
     assert_equal [{:dog=>"fido"}, {:stuff=>"things"}], hashes
   end
 
